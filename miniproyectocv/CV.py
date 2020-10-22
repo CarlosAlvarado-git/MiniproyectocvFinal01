fea469637291a4fce141fa0047858ed0aceb8486
Carlos Alvarado <cmalvarado@ufm.edu>
2020-10-22 10:44:40 -0600

    Update

---
+++ miniproyectocv/CV.py
@@ -0,0 +1,20 @@
+from flask import Flask, url_for
+from jinja2 import Template, Environment, FileSystemLoader
+import yaml
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+app = Flask(__name__)
+with open("MICV.yaml") as yaml_file:
+    miyaml = yaml.load(yaml_file)
+@app.route('/CV')
+def CV():
+    template = env.get_template('CV_.html')
+    foto_mia = url_for('static', filename=miyaml['fotografia'])
+    return template.render(my_data=miyaml, foto_mia=foto_mia)
+@app.route('/')
+def Linker():
+    template = env.get_template('Linker.html')
+    return template.render(my_data=miyaml, links=miyaml['Redes_Sociales'])
+
+if __name__ == '__main__':
+    app.run()
---
+++ miniproyectocv/README.md
@@ -0,0 +1,3 @@
+Docker Hub(https://hub.docker.com/repository/docker/carlosalvaradodock/micvfinal01)
+'RUN'
+    'docker run -i -t -p 5000:5000 carlosalvaradodock/micvfinal01'
